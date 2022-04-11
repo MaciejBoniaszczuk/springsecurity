fc77849c6a3cabaa046c0fed716e513d5561a7fe
Maciej Boniaszczuk <52282101+MaciejBoniaszczuk@users.noreply.github.com>
2022-04-11 19:43:02 +0200

    Delete 01/springsecurity directory

--- 01/springsecurity/src/main/java/pl/boniaszczuk/springsecurity/repo/AppUserRepo.java
+++
@@ -1,9 +0,0 @@
-package pl.boniaszczuk.springsecurity.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.boniaszczuk.springsecurity.entity.AppUser;
-
-public interface AppUserRepo extends JpaRepository<AppUser, Long> {
-
-    AppUser findAllByUsername(String username);
-}
